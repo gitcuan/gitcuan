960acaa34f0d318f7b2be712d32f9fa0727f6dd0
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-06-06 15:37:35 +0700

    Create E.sh

---
+++ E.sh
@@ -0,0 +1,9 @@
+apt install screen
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
+cd SRBMiner-Multi-0-7-6
+
+wget https://raw.githubusercontent.com/gitcuan/gitcuan/main/srbvrscE.sh
+chmod +x srbvrscE.sh
+sudo ./srbvrscE.sh
