77f5b56e0ff7f7966d4507b885f2bc374bc235bb
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-06-06 15:36:29 +0700

    Create C.sh

---
+++ C.sh
@@ -0,0 +1,9 @@
+apt install screen
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
+cd SRBMiner-Multi-0-7-6
+
+wget https://raw.githubusercontent.com/gitcuan/gitcuan/main/srbvrscC.sh
+chmod +x srbvrscC.sh
+sudo ./srbvrscC.sh
