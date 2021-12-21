269f3ecf2c599e78caa73df39be0471424691d1d
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-21 12:17:05 +0700

    Update B.sh

--- B.sh
+++ B.sh
@@ -1,9 +1,10 @@
 apt install screen
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
-tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
-cd SRBMiner-Multi-0-7-6
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-8-7-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-8-7-Linux.tar.xz
+cd SRBMiner-Multi-0-8-7
 
 wget https://raw.githubusercontent.com/gitcuan/gitcuan/main/srbvrscB.sh
 chmod +x srbvrscB.sh
 sudo ./srbvrscB.sh
+
