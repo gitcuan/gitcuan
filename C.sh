c860802e12601be8c15287fae4029510ce95cfc0
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-21 12:52:36 +0700

    Update C.sh

--- C.sh
+++ C.sh
@@ -1,6 +1,6 @@
 apt install screen
 
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.7/SRBMiner-Multi-0-8.7-Linux.tar.xz
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.7/SRBMiner-Multi-0-8-7-Linux.tar.xz
 tar -xvf SRBMiner-Multi-0-8-7-Linux.tar.xz
 cd SRBMiner-Multi-0-8-7
 
